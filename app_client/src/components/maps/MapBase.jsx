0245144b422a0406010ad2d8e5d42910a4230a49
ahugofonseca <a.hugofonseca@gmail.com>
2020-04-17 01:52:33 -0300

    Customize Tooltip and Marker

--- app_client/src/components/maps/MapBase.jsx
+++ app_client/src/components/maps/MapBase.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import Boxes from '../ubs/Boxes';
 import L from 'leaflet'
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Map, TileLayer, Marker, Tooltip, Popup } from 'react-leaflet';
 import './Maps.scss'
 
-export const unselectedIcon = new L.Icon({
-  iconUrl: '/unselected-marker.png',
-  iconSize: [50, 65]
+export const unselectedIcon = new L.DivIcon({
+    className: 'my-div-icon',
+    html: '<img class="marker-icon" src="/unselected-marker.png" height="75" />'+
+          '<span class="marker-label">1</span>'
 })
 
-export const selectedPoint = new L.Icon({
-  iconUrl: '/selected-marker.png',
-  iconSize: [50, 65]
+export const selectedPoint = new L.DivIcon({
+    className: 'my-div-icon',
+    html: '<img class="marker-icon" src="/selected-marker.png" height="75" />'+
+          '<span class="marker-label">1</span>'
 })
 
 class MapBase extends Component {
@@ -37,9 +39,15 @@ class MapBase extends Component {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
 
-          <Marker position={position} icon={unselectedIcon}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
+          <Marker position={position} icon={unselectedIcon} alt="1">
+            <Popup className="custom-popup">
+              <div className="popup-title">
+                <span><b> UBS Jardim Europa </b></span>
+              </div>
+
+              <div className="popup-text">
+                <span> Rua Pascal, 1382 - Jardim Europa - SP </span>
+              </div>
             </Popup>
           </Marker>
         </Map>
