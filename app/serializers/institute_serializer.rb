84b0a251ee78b4fbeaade35d18b12e9e96710cd0
ahugofonseca <a.hugofonseca@gmail.com>
2020-04-17 03:48:12 -0300

    Apply mask in phone number

--- app/serializers/institute_serializer.rb
+++ app/serializers/institute_serializer.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class InstituteSerializer < ApplicationSerializer
-  attributes :id, :name, :address, :city, :phone, :geocode
+  attributes :id, :name, :address, :city, :geocode
+
+  attributes :phone do |object|
+    "(#{object.phone.first(2)}) #{object.phone.first(7).last(5)}-#{object.phone.last(4)}"
+  end
 end
